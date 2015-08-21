aa389a9b2af9d81ee9e4f709ef4af0cbcd69c602
Ben Spenard <bdspen@gmail.com>
2015-08-21 11:38:47 -0700

    add getters/setters in Client.php tests pass

--- src/Client.php
+++ src/Client.php
@@ -1,3 +1,25 @@
 <?php
-    
+    class Client
+    {
+        private $client_name;
+        private $id;
+
+        function __construct($client_name, $id = null)
+        {
+            $this->client_name = $client_name;
+            $this->id = $id;
+        }
+        function setClientName($new_client_name)
+        {
+            $this->client_name = (string) $new_client_name;
+        }
+        function getClientName()
+        {
+            return $this->client_name;
+        }
+        function getId()
+        {
+            return $this->id;
+        }
+    }
  ?>
